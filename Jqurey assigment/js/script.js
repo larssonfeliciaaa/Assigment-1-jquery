cf4fbce642e310b702e9407fa83192c076c1ea27
Felicia Larsson <56109849+larssonfeliciaaa@users.noreply.github.com>
2020-03-11 14:01:03 +0100

    Add files via upload

---
+++ Jqurey assigment/js/script.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function() {
+
+  let count =0;
+  const boxes = $("li");
+  
+  
+  // Check All
+  $(".checkall").click(function() {
+      $(":checkbox").attr("checked", true);
+  });
+
+  // Uncheck All
+  $(".uncheckall").click(function() {
+      $(":checkbox").attr("checked", false);
+  });
+
+  $("#btn-clear").click(function(){
+   $(".check-b li input:checked").parent().css("background-color","grey");
+
+  });
+
+  $(".color-btn").click(function(event){
+    let color = $(this).css("background-color"); 
+
+
+    $("input:checked").parent().css("background-color", color);
+  });
+
+  $("#btn-apply").click(function(){
+      var str = $("#myText").val();
+      $("input:checked").next("span").text(str);
+  });
+
+
+  $("#btn-rem").click(function(){
+    let removed = $("input:checked").parent().detach();
+    count += removed.length;
+    $(".span-counter").text(count);
+  });
+
+
+  $("#btn-res").click(function(){
+     $("ul").append(boxes);
+     count=0;
+     $(".span-counter").text(count);
+  }); 
+}); 
+
+
+
+
+
